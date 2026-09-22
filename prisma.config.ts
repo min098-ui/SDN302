f6835b138230d4df23ce739b17d6d01a79309cc2
gi098 <gi098@users.noreply.github.com>
2026-09-22 17:04:39 +0700

    ci: set global DATABASE_URL in CI and add config fallback

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
+    url:
+      process.env.DATABASE_URL ||
+      "postgresql://postgres:postgres@localhost:5432/postgres",
   },
 });
